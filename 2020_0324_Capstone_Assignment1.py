b418ec663cfc23efa28ac620379cac5b0f248845
johnkn31 <30707798+johnkn31@users.noreply.github.com>
2020-03-24 20:41:17 -0400

    Add files via upload

---
+++ 2020_0324_Capstone_Assignment1.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # This notebook will be used for capstone project.
+# 
+
+# In[6]:
+
+
+import pandas as pd
+
+
+# In[7]:
+
+
+import numpy as np
+
+
+# In[9]:
+
+
+print("Hello Capstone Project Course!")
+
